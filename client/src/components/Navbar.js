8f7beafd67916c7e3d047b848fd448cbde0e5351
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-06-27 20:03:42 +0300

    init: add drop-down filter menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -11,8 +11,16 @@ import Menu from '@material-ui/core/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import { Link } from 'react-router-dom';
-
 import FilterListIcon from '@material-ui/icons/FilterList';
+
+
+
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+
+
 
 import '../styles/navbar.scss'
 
@@ -79,6 +87,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
 export const Navbar = () => {
   const classes = useStyles();
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
@@ -122,12 +165,37 @@ export const Navbar = () => {
     </Menu>
   );
 
+
+
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  // const handleChange = (event) => {
+  //   setAuth(event.target.checked);
+  // };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  // DROP DOWN MENU
+  const [age, setAge] = React.useState('');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div className={classes.grow}>
-      <AppBar>
+      <AppBar style={{display: "block"}}>
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
-            Online Game Shop
+            <Link to='/' className='navbar-links'>Online Game Shop</Link>
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -146,6 +214,7 @@ export const Navbar = () => {
           <IconButton
                 aria-controls="menu-appbar"
                 color="inherit"
+                onClick={handleMenu}
               >
                 <FilterListIcon/>
             </IconButton>
@@ -176,11 +245,103 @@ export const Navbar = () => {
               <MoreIcon />
             </IconButton>
           </div>
+
+
+            
+          <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem> */}
+
+                <div className='filter-block'>
+                  <div className='filter-block__filter'>
+                      <h2>Filter</h2>
+
+                      <div className='filter-block__filter-option'>
+                        <span className='filter-block__filter-option__title'>Game Name:</span>
+                        <FormControl className={classes.margin}>
+                          <InputLabel htmlFor="demo-customized-textbox">Game Name</InputLabel>
+                          <BootstrapInput id="demo-customized-textbox" />
+                        </FormControl>
+                      </div>
+
+                      <div className='filter-block__filter-option'>
+                        <span className='filter-block__filter-option__title'>Author:</span>
+                        <FormControl className={classes.margin}>
+                          <InputLabel htmlFor="demo-customized-textbox">Author</InputLabel>
+                          <BootstrapInput id="demo-customized-textbox" />
+                        </FormControl>
+                      </div>
+
+                      <div className='filter-block__filter-option'>
+                        <span className='filter-block__filter-option__title'>Genre:</span>
+                        <FormControl className={classes.margin}>
+                          <InputLabel htmlFor="demo-customized-textbox">Genre</InputLabel>
+                          <BootstrapInput id="demo-customized-textbox" />
+                        </FormControl>
+                      </div>
+
+                      <div className='filter-block__filter-option'>
+                        <span className='filter-block__filter-option__title'>Number of copies:</span>
+                        <FormControl className={classes.margin}>
+                          <InputLabel htmlFor="demo-customized-textbox">Number of copies</InputLabel>
+                          <BootstrapInput id="demo-customized-textbox" />
+                        </FormControl>
+                      </div>
+                  </div>
+
+                  <div className='filter-block__sort'>
+                    <h2>Sorting</h2>
+
+                    <div className='filter-block__filter-option'>
+                      <span className='filter-block__filter-option__title'>Select sort:</span>
+                      <FormControl className={classes.margin}>
+                        <InputLabel id="demo-customized-select-label">Sorting</InputLabel>
+                        <Select
+                          labelId="demo-customized-select-label"
+                          id="demo-customized-select"
+                          value={age}
+                          onChange={handleChange}
+                          input={<BootstrapInput />}
+                        >
+                          <MenuItem value="">
+                            <em>None</em>
+                          </MenuItem>
+                          <MenuItem value={'popular'}>Popular</MenuItem>
+                          <MenuItem value={'price'}>Price</MenuItem>
+                          <MenuItem value={'new'}>New</MenuItem>
+                          <MenuItem value={'discount'}>Discount</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </div>
+
+
+
+                  </div>
+                </div>
+              </Menu>
+
+
+
         </Toolbar>
       </AppBar>
       
       {renderMobileMenu}
 
     </div>
+      
   );
 }
