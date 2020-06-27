772fa699ee58e3eda9190bda7aa29f4059a2f314
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-06-27 18:10:35 +0300

    changes: add product cards

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import '../styles/content.scss'
 
 export const HomePage = () => {
 
+    let goods = [1,2,3,4,5,6,7,8,9,10];
+
     return (
-        <h1>Home Page</h1>
+        <main className='container'>
+            <div className='content'>
+
+                {
+                    goods.map((item, index) => {
+
+                        return (
+                            <button className='content__card-btn' key={index}>
+                                <div className='content__card'>
+                                    <div className='content__card__picture'></div>
+                                    <div className='content__card_description'>
+                                        <h2>{item}</h2>
+                                        <h3>Genre</h3>
+                                        <h3>Author</h3>
+                                        <p>Price</p>
+                                    </div>
+                                </div>
+                            </button>
+                        )
+                    })
+                }
+            </div>
+        </main>
     )
 }
