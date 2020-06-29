aa5c8d869a762734e3604cdf180cf3a3fb075398
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-06-30 01:14:10 +0300

    changes: change paddingLeft in inputInput

--- client/src/hooks/useStyles.js
+++ client/src/hooks/useStyles.js
@@ -42,7 +42,7 @@ export const useStyles = makeStyles((theme) => ({
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    paddingLeft: theme.spacing(6),
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
