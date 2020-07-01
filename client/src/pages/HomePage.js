6f6b07cedfecc49094d9f0ab94a885e6a67f3ac7
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-01 19:29:33 +0300

    changes: add content to the home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import '../styles/content.scss'
+import React from "react";
+import "../styles/content.scss";
 
 export const HomePage = () => {
 
-    let goods = [1,2,3,4,5,6,7,8,9,10];
+  const games = [
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 1},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 2},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 3},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 4},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 5},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 6},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 7},
+    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 8},
+  ]
 
-    return (
-        <main className='container'>
-            <div className='content'>
-                {
-                    goods.map((item, index) => {
-
-                        return (
-                            <button className='content__card-btn' key={index}>
-                                <div className='content__card'>
-                                    <div className='content__card__picture'></div>
-                                    <div className='content__card_description'>
-                                        <h2>{item}</h2>
-                                        <h3>Genre</h3>
-                                        <h3>Author</h3>
-                                        <p>Price</p>
-                                    </div>
-                                </div>
-                            </button>
-                        )
-                    })
-                }
-
-                <div>
-                    
+  return (
+    <main className="container">
+      <div className="content">
+        {games.map((game) => {
+          return (
+            <button className="content__card-btn" key={game.id}>
+              <div className="content__card">
+                <div className="content__card__picture"></div>
+                <div className="content__card_description">
+                  <h2>{game.gameName}</h2>
+                  <h3>{game.genre}</h3>
+                  <h3>{game.author}</h3>
+                  <p>{game.price} $</p>
                 </div>
-            </div>
-        </main>
-    )
-}
+              </div>
+            </button>
+          );
+        })}
+
+        <div></div>
+      </div>
+    </main>
+  );
+};
