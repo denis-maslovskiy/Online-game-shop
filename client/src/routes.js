6db3722990609729e529446bbb29d3826c050402
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-03 16:57:24 +0300

    init: add Basket route

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route} from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { Registration } from './pages/Registration';
 import { SelectedGame } from './pages/SelectedGame';
+import { Basket } from './pages/Basket';
 
 export const useRoutes = () => {
 
@@ -19,6 +20,10 @@ export const useRoutes = () => {
 
             <Route path='/selectedgame' exact>
                 <SelectedGame/>
+            </Route>
+
+            <Route path='/basket'>
+                <Basket/>
             </Route>
 
         </Switch>
