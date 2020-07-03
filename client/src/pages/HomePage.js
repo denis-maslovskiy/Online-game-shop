0c9fbd8c573566a1570f83c6491aa136826ac1a1
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-03 16:54:22 +0300

    changes: renamed BEM classes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,18 +1,66 @@
 import React from "react";
+import image from "../img/3.jpg";
 import "../styles/content.scss";
 
 export const HomePage = () => {
-
   const games = [
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 1},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 2},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 3},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 4},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 5},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 6},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 7},
-    {gameName: 'Some kind of game', genre: 'Action', author: 'Artificial Intelligence', price: '20', id: 8},
-  ]
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 1,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 2,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 3,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 4,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 5,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 6,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 7,
+    },
+    {
+      gameName: "Some kind of game",
+      genre: "Action",
+      author: "Artificial Intelligence",
+      price: "20",
+      id: 8,
+    },
+  ];
 
   return (
     <main className="container">
@@ -20,9 +68,9 @@ export const HomePage = () => {
         {games.map((game) => {
           return (
             <button className="content__card-btn" key={game.id}>
-              <div className="content__card">
-                <div className="content__card__picture"></div>
-                <div className="content__card_description">
+              <div className="content__card card">
+                <img src={image} className="card__picture" />
+                <div className="card_description">
                   <h2>{game.gameName}</h2>
                   <h3>{game.genre}</h3>
                   <h3>{game.author}</h3>
