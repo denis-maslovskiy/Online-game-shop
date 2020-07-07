fae0011f97300a9a1bcfc0e5e103f23e4c5e8ea8
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-07 23:34:19 +0300

    fix: renamed useHttp hook import && complete registration logic

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,71 +1,92 @@
-import React from 'react';
-import { Formik, useField, Form } from 'formik';
-import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
-import '../styles/auth.scss'
+import React from "react";
+import { Formik, useField, Form } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
+import { useHttp } from '../hooks/httpHook';
+import "../styles/auth.scss";
 
 const CustomTextInput = ({ label, ...props }) => {
-    const [field, meta] = useField(props);
-    
+  const [field, meta] = useField(props);
 
-    return (
-        <>
-            <label htmlFor={props.id || props.name}>{label}</label>
-            <input {...field} {...props}/>
-            {meta.touched && meta.error ? (
-                <div>{meta.error}</div>
-            ) : null}
-        </>
-    )
-}
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input {...field} {...props} />
+      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+    </>
+  );
+};
 
 export const Registration = () => {
+    const {loading, request} = useHttp();
 
-    const schema = Yup.object({
-        userName: Yup.string().min(3, 'Must be at least 3 characters').max(15, 'Must be no more than 15 characters').required('Required'),
-        email: Yup.string().email('Invalid email address').required('Required'),
-        password: Yup.string().min(6, 'Must be at least 6 characters').required('Required')
-    });
+  const schema = Yup.object({
+    username: Yup.string()
+      .min(3, "Must be at least 3 characters")
+      .max(15, "Must be no more than 15 characters")
+      .required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string()
+      .min(6, "Must be at least 6 characters")
+      .required("Required"),
+  });
 
-    const submit = (values, { setSubmitting, resetForm }) => {
-        setTimeout(() => {
-            console.log(JSON.stringify(values));
-            resetForm();
-            setSubmitting(false);
-        }, 3000);
-       
-    };
+  const submit = async (values, { setSubmitting, resetForm }) => {
+    try {
+        const data = await request('/api/auth/registration', 'POST', {...values});
+        console.log('Data: ', data);
+        resetForm();
+        setSubmitting(false);
+    } catch (e) {}
+  };
 
-    return (
-            <Formik
-                initialValues={{
-                    userName: "",
-                    email: "",
-                    password: "",
-                }}
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={schema}
+      onSubmit={submit}
+    >
+      {(props) => (
+        <Form className="box">
+          <h1 className="box__title">Registration</h1>
 
-                validationSchema = {schema}
+          <CustomTextInput
+            className="box__input"
+            name="username"
+            type="text"
+            placeholder="Username"
+          />
 
-                onSubmit = {submit}
-            >
-                {props => (
-                    <Form className='box'>
-                        <h1 className="box__title">Registration</h1>
+          <CustomTextInput
+            className="box__input"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
 
-                        
-                        <CustomTextInput className='box__input' name='userName' type='text' placeholder='Username' autoComplete='off' />
-                        
-                        <CustomTextInput className='box__input' name='email' type='text' placeholder='Email' autoComplete='off'/>
+          <CustomTextInput
+            className="box__input"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
 
-                        <CustomTextInput className='box__input' name='password' type='password' placeholder='Password' autoComplete='off'/>
+          <button className="box__submit-btn" type="submit" disabled={loading}>
+            {props.isSubmitting ? "Loading..." : "Sign Up"}
+          </button>
 
-                        <button className='box__submit-btn' type='submit'>{props.isSubmitting ? 'Loading...' : 'Sign Up'}</button>
-
-                        <div className='box__bottom-text'>
-                            Have an account? <Link to='/authorization' className='box__bottom-text__link'>Log in</Link>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-    )
-}
+          <div className="box__bottom-text">
+            Have an account?{" "}
+            <Link to="/authorization" className="box__bottom-text__link">
+              Log in
+            </Link>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
