75a9d269685518b008b241f49ce08475313323ad
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-07 23:32:51 +0300

    init: add hook for login/logout

---
+++ client/src/hooks/authHook.js
@@ -0,0 +1,30 @@
+import { useState, useCallback, useEffect } from 'react';
+
+export const useAuth = () => {
+    const [token, setToken] = useState(null);
+    const [userId, setUserId] = useState(null);
+
+    const login = useCallback((jwtToken, id) => {
+        setToken(jwtToken);
+        setUserId(id);
+
+        localStorage.setItem('userData', JSON.stringify({ userId, token}));
+    }, [])
+
+    const logout = useCallback(() => {
+        setToken(null);
+        setUserId(null);
+
+        localStorage.removeItem('userData');
+    }, [])
+
+    useEffect(() => {
+        const data = JSON.parse(localStorage['userData']);
+
+        if(data && data.token) {
+            login(data.token, data.userId);
+        }
+    }, [login]);
+
+    return { login, logout, token, userId }
+}
