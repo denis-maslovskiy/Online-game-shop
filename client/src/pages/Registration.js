2e97ddc3bfe9363f1ab9445d153923657df0e1c1
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-08 23:39:01 +0300

    changes: add visual error/account create  display

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Formik, useField, Form } from "formik";
 import * as Yup from "yup";
+import ClearIcon from "@material-ui/icons/Clear";
 import { Link } from "react-router-dom";
-import { useHttp } from '../hooks/httpHook';
+import { useHttp } from "../hooks/httpHook";
 import "../styles/auth.scss";
+import "../styles/alert.scss";
 
 const CustomTextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
@@ -17,8 +19,45 @@ const CustomTextInput = ({ label, ...props }) => {
   );
 };
 
+const Alert = ({ status }) => {
+  const alert = useRef();
+
+  const clickHandler = () => {
+    alert.current.classList.remove("show");
+    alert.current.classList.add("hide");
+  };
+
+  if (status === "User created") {
+    return (
+      <div ref={alert} className="alert alert-success show">
+        <span className="alert__text">{status}! Now you can Log In.</span>
+        <button
+          className="alert__close-btn alert__close-btn_success"
+          onClick={clickHandler}
+        >
+          <ClearIcon />
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div ref={alert} className="alert alert-error show">
+        <span className="alert__text">{status}</span>
+        <button
+          className="alert__close-btn alert__close-btn_error"
+          onClick={clickHandler}
+        >
+          <ClearIcon />
+        </button>
+      </div>
+    );
+  }
+};
+
 export const Registration = () => {
-    const {loading, request} = useHttp();
+  const { loading, request, error, clearError } = useHttp();
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [successStatus, setSuccessStatus] = useState("");
 
   const schema = Yup.object({
     username: Yup.string()
@@ -32,61 +71,78 @@ export const Registration = () => {
   });
 
   const submit = async (values, { setSubmitting, resetForm }) => {
+    setIsSubmit(true);
     try {
-        const data = await request('/api/auth/registration', 'POST', {...values});
-        console.log('Data: ', data);
-        resetForm();
-        setSubmitting(false);
+      const data = await request("/api/auth/registration", "POST", {
+        ...values,
+      });
+      setSuccessStatus(data.message);
+      resetForm();
+      setSubmitting(false);
     } catch (e) {}
+    setTimeout(() => setSuccessStatus(null), 3000);
+    setTimeout(() => clearError(), 3000);
   };
 
   return (
-    <Formik
-      initialValues={{
-        username: "",
-        email: "",
-        password: "",
-      }}
-      validationSchema={schema}
-      onSubmit={submit}
-    >
-      {(props) => (
-        <Form className="box">
-          <h1 className="box__title">Registration</h1>
+    <>
+      {isSubmit && successStatus ? (
+        <Alert status={successStatus} />
+      ) : error ? (
+        <Alert status={error} />
+      ) : null}
 
-          <CustomTextInput
-            className="box__input"
-            name="username"
-            type="text"
-            placeholder="Username"
-          />
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={schema}
+        onSubmit={submit}
+      >
+        {(props) => (
+          <Form className="box">
+            <h1 className="box__title">Registration</h1>
 
-          <CustomTextInput
-            className="box__input"
-            name="email"
-            type="text"
-            placeholder="Email"
-          />
+            <CustomTextInput
+              className="box__input"
+              name="username"
+              type="text"
+              placeholder="Username"
+            />
 
-          <CustomTextInput
-            className="box__input"
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
+            <CustomTextInput
+              className="box__input"
+              name="email"
+              type="text"
+              placeholder="Email"
+            />
 
-          <button className="box__submit-btn" type="submit" disabled={loading}>
-            {props.isSubmitting ? "Loading..." : "Sign Up"}
-          </button>
+            <CustomTextInput
+              className="box__input"
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
 
-          <div className="box__bottom-text">
-            Have an account?{" "}
-            <Link to="/authorization" className="box__bottom-text__link">
-              Log in
-            </Link>
-          </div>
-        </Form>
-      )}
-    </Formik>
+            <button
+              className="box__submit-btn"
+              type="submit"
+              disabled={loading}
+            >
+              {props.isSubmitting ? "Loading..." : "Sign Up"}
+            </button>
+
+            <div className="box__bottom-text">
+              Have an account?{" "}
+              <Link to="/authorization" className="box__bottom-text__link">
+                Log in
+              </Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
