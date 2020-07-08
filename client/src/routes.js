acb8653fefb209811e3acf503e7193dff925aa28
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-08 23:33:49 +0300

    changes: implemented access to routes regarding user authentication

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Registration } from "./pages/Registration";
 import { SelectedGame } from "./pages/SelectedGame";
@@ -7,7 +7,31 @@ import { Basket } from "./pages/Basket";
 import { PersonalAccount } from "./pages/PersonalAccount";
 import { Authorization } from "./pages/Authorization";
 
-export const useRoutes = () => {
+export const useRoutes = (isAuthenticated) => {
+  if (isAuthenticated) {
+    return (
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="selectedgame" exact>
+          <SelectedGame />
+        </Route>
+
+        <Route path="/basket" exact>
+          <Basket />
+        </Route>
+
+        <Route path="/account" exact>
+          <PersonalAccount />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
   return (
     <Switch>
       <Route path="/" exact>
@@ -22,17 +46,11 @@ export const useRoutes = () => {
         <SelectedGame />
       </Route>
 
-      <Route path="/basket">
-        <Basket />
-      </Route>
-
-      <Route path="/account">
-        <PersonalAccount />
-      </Route>
-
       <Route path="/authorization" exact>
         <Authorization />
       </Route>
+
+      <Redirect to="/authorization" />
     </Switch>
   );
 };
