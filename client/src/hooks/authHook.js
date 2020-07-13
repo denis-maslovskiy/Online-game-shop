c7158517410398e03abac083029d6bc83549f671
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-13 19:42:04 +0300

    changes: prettie

--- client/src/hooks/authHook.js
+++ client/src/hooks/authHook.js
@@ -1,30 +1,33 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from "react";
 
 export const useAuth = () => {
-    const [token, setToken] = useState(null);
-    const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
 
-    const login = useCallback((jwtToken, id) => {
-        setToken(jwtToken);
-        setUserId(id);
+  const login = useCallback((jwtToken, id) => {
+    setToken(jwtToken);
+    setUserId(id);
 
-        localStorage.setItem('userData', JSON.stringify({ userId: id, token: jwtToken}));
-    }, [])
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({ userId: id, token: jwtToken })
+    );
+  }, []);
 
-    const logout = useCallback(() => {
-        setToken(null);
-        setUserId(null);
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
 
-        localStorage.removeItem('userData');
-    }, [])
+    localStorage.removeItem("userData");
+  }, []);
 
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem('userData'));
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("userData"));
 
-        if(data && data.token) {
-            login(data.token, data.userId);
-        }
-    }, [login]);
+    if (data && data.token) {
+      login(data.token, data.userId);
+    }
+  }, [login]);
 
-    return { login, logout, token, userId }
-}
+  return { login, logout, token, userId };
+};
