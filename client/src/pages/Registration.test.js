eef173dd6048a416fb496e09316e97be5dea3d09
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-21 21:53:26 +0300

    changes: create first working test

--- client/src/pages/Registration.test.js
+++ client/src/pages/Registration.test.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom'
 import Registration from './Registration';
 
 describe('Registration test: ', () => {
-    const wrapper = shallow(<Registration></Registration>);
+    const wrapper = shallow(<MemoryRouter><Registration/></MemoryRouter>);
 
     it('should exist: ', () => {
         expect(wrapper).toExist();
     });
+
+    it('to be defined: ', () => {
+        expect(wrapper).toBeDefined();
+    });
+
+    // wrapper.find('.box__submit-btn').simulate('click');
+    console.log('Log: ', wrapper.find('.box__submit-btn'))
+    // console.log(wrapper.find('.box__submit-btn').text())
+
+
 })
