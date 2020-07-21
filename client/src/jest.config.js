26197e1610eb1b8b99b6ec50f1ea63eeb3cb0729
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-07-21 21:52:41 +0300

    updated

--- client/src/jest.config.js
+++ client/src/jest.config.js
@@ -1,34 +1,49 @@
 module.exports = {
-    // Automatically clear mock calls and instances between every test
-    clearMocks: true,
-  
-    // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
-  
-    // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
-  
-    // An array of file extensions your modules use
-    moduleFileExtensions: ['js', 'json', 'jsx'],
-  
-    // The paths to modules that run some code to configure or set up the testing environment before each test
-    setupFiles: ['<rootDir>/enzyme.config.js'],
-  
-    // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
-  
-    // The glob patterns Jest uses to detect test files
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  
-    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
-  
-    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-    testURL: 'http://localhost',
-  
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-    
-    // Indicates whether each individual test should be reported during the run
-    verbose: false,
-  };
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      diagnostics: true,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/serviceWorker.ts',
+    '!src/setupTests.ts',
+    '!src/index.tsx',
+  ],
+  setupFiles: ['./config/jest/setupJest.ts', './config/jest/setupEnzyme.ts'],  
+  coveragePathIgnorePatterns: ['./src/*/*.types.{ts,tsx}'],
+  coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      lines: 95,
+      functions: 95,
+    },
+  },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: ['<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'],
+  automock: false,
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/ts-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
+      '<rootDir>/config/jest/fileTransform.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/ts-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
+      '<rootDir>/config/jest/fileTransform.js',
+  },
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    'src/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
