95028d0c05ddf3eab41786041c8e701384cd1ed2
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-08-05 20:49:36 +0300

    changes: add logic for info message in Notification.js

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,7 +1,11 @@
 import React, { useRef } from "react";
 import ClearIcon from "@material-ui/icons/Clear";
 import { connect } from "react-redux";
-import { clearErrorMessage } from "../redux/notification/notificationActions";
+import {
+  clearErrorMessage,
+  clearSuccessMessage,
+  clearInfoMessage,
+} from "../redux/notification/notificationActions";
 import "../styles/notification.scss";
 
 const Notification = (props) => {
@@ -11,14 +15,14 @@ const Notification = (props) => {
     alertRef.current.classList.remove("show");
     alertRef.current.classList.add("hide");
     props.clearErrorMessage();
+    props.clearSuccessMessage();
+    props.clearInfoMessage();
   };
 
   if (props.values.successMsg) {
     return (
       <div ref={alertRef} className="alert alert-success show">
-        <span className="alert__text">
-          {props.values.successMsg}
-        </span>
+        <span className="alert__text">{props.values.successMsg}</span>
         <button
           className="alert__close-btn alert__close-btn--success"
           onClick={clickHandler}
@@ -27,7 +31,7 @@ const Notification = (props) => {
         </button>
       </div>
     );
-  } 
+  }
   if (props.values.errorMsg) {
     return (
       <div ref={alertRef} className="alert alert-error show">
@@ -40,12 +44,27 @@ const Notification = (props) => {
         </button>
       </div>
     );
+  }
+  if (props.values.infoMsg) {
+    return (
+      <div ref={alertRef} className="alert alert-info show">
+        <span className="alert__text">{props.values.infoMsg}</span>
+        <button
+          className="alert__close-btn alert__close-btn--info"
+          onClick={clickHandler}
+        >
+          <ClearIcon />
+        </button>
+      </div>
+    );
   } else return null;
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     clearErrorMessage: () => dispatch(clearErrorMessage()),
+    clearSuccessMessage: () => dispatch(clearSuccessMessage()),
+    clearInfoMessage: () => dispatch(clearInfoMessage()),
   };
 };
 
