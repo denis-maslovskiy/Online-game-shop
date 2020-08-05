834a9df2b948422560444fe5e83dd703012a04be
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-08-05 20:58:30 +0300

    changes: added logic of info message

--- client/src/redux/notification/notificationReducer.js
+++ client/src/redux/notification/notificationReducer.js
@@ -1,13 +1,17 @@
 import {
-  AXIOS_SUCCESS_MESSAGE,
-  AXIOS_ERROR_MESSAGE,
+  SUCCESS_MESSAGE,
+  ERROR_MESSAGE,
+  INFO_MESSAGE,
   CLEAR_ERROR_MESSAGE,
+  CLEAR_SUCCESS_MESSAGE,
+  CLEAR_INFO_MESSAGE,
 } from "./notificationTypes";
 import { updateObject } from "../reducerHelpers";
 
 const initialState = {
   errorMsg: "",
   successMsg: "",
+  infoMsg: "",
 };
 
 const setErrorMessage = (state, action) => {
@@ -20,21 +24,45 @@ const setSuccessMessage = (state, action) => {
   return updateObject(state, { successMsg });
 };
 
+const setInfoMessage = (state, action) => {
+  const infoMsg = action.payload;
+  return updateObject(state, { infoMsg });
+};
+
 const clearErrorMessage = (state) => {
   const errorMsg = "";
   return updateObject(state, { errorMsg });
 };
 
+const clearSuccessMessage = (state) => {
+  const successMsg = "";
+  return updateObject(state, { successMsg });
+};
+
+const clearInfoMessage = (state) => {
+  const infoMsg = "";
+  return updateObject(state, { infoMsg });
+};
+
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
-    case AXIOS_ERROR_MESSAGE:
+    case ERROR_MESSAGE:
       return setErrorMessage(state, action);
 
-    case AXIOS_SUCCESS_MESSAGE:
+    case SUCCESS_MESSAGE:
       return setSuccessMessage(state, action);
+
+    case INFO_MESSAGE:
+      return setInfoMessage(state, action);
 
     case CLEAR_ERROR_MESSAGE:
       return clearErrorMessage(state);
+
+    case CLEAR_SUCCESS_MESSAGE:
+      return clearSuccessMessage(state);
+
+    case CLEAR_INFO_MESSAGE:
+      return clearInfoMessage(state);
 
     default:
       return state;
