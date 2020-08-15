837ea0e6bd365da974ef82272245fd6720c41aed
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2020-08-15 17:17:42 +0300

    changes: remove unused hook

--- client/src/hooks/authHook.js
+++ client/src/hooks/authHook.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const useAuth = () => {
   const [token, setToken] = useState(null);
