f0620de50e655915954ab26719c17e372df3df5e
Dzianis Maslouski <Dzianis.Maslouski@itechart-group.com>
2020-08-17 16:03:17 +0300

    save

--- client/src/redux/authentication/authenticationReducer.js
+++ client/src/redux/authentication/authenticationReducer.js
@@ -2,25 +2,18 @@ import { SET_CURRENT_USER } from "./authenticationTypes";
 import { updateObject } from "../reducerHelpers";
 
 const initialState = {
-  username: "",
-  email: "",
-  userId: JSON.parse(localStorage.getItem('userData')) ? JSON.parse(localStorage.getItem('userData')).userId : "",
-  purchasedGames: [],
-  dateOfRegistration: "",
+  user: {},
   token: JSON.parse(localStorage.getItem('userData')) ? JSON.parse(localStorage.getItem('userData')).token : ""
 };
 
 const setCurrentUser = (state, action) => {
   const { user, token } = action.payload;
-  const username = user.name,
-    email = user.email,
-    userId = user.id,
-    dateOfRegistration = user.dateOfRegistration;
 
-  return updateObject(state, { username, email, userId, dateOfRegistration, token });
+  return updateObject(state, { ...user, token });
 };
 
 const authenticationReducer = (state = initialState, action) => {
+  console.log(action);
   switch (action.type) {
     case SET_CURRENT_USER:
       return setCurrentUser(state, action);
