988c2319da7881a7a12bafdc8f6b4ad60a3487ee
Dzianis Maslouski <Dzianis.Maslouski@itechart-group.com>
2020-09-29 20:51:30 +0300

    added new reducer event | prettier code | remove useless code

--- client/src/redux/games/gamesReducer.js
+++ client/src/redux/games/gamesReducer.js
@@ -1,43 +1,45 @@
-import { SET_ALL_GAMES, SET_GAME_DATA, UPDATE_GAME_ARRAY } from './gamesTypes';
-import { updateObject } from '../reducerHelpers';
+import { SET_ALL_GAMES, UPDATE_GAME_ARRAY, DELETE_GAME } from "./gamesTypes";
 
 const initialState = {
-    allGames: [],
-    game: {}
-}
-
-// const setAllGames = (action, state) => { //? am i need this? 
-//     const allGames = action.payload;
-//     updateObject(state, { allGames })
-// }
-
-// need to remove / 26.09 - but its used //? should i really remove this? Need to check it
-const setGameData = (action, state) => {
-    const game = action.payload;
-    updateObject(state, { ...game })
-}
+  allGames: [],
+  game: {},
+};
 
 const updateGameArray = (action, state) => {
-    const game = action.payload;
-    console.log('game: ', game);
-    // в массиве найти эту игру по id и заменить на новую
-    // updateObject(state, { ...game })
-}
+  const updatedGame = action.payload;
+  state.allGames.map((game, index) => {
+    if (game._id === updatedGame._id) {
+      state.allGames[index] = updatedGame;
+    }
+    return game;
+  });
+  return state;
+};
+
+const updateGameArrayAfterDeletingTheGame = (action, state) => {
+  state.allGames.map((game, index) => {
+    if (game._id === action.payload) {
+      state.allGames.splice(index, 1);
+    }
+    return game;
+  });
+  return state;
+};
 
 const gamesReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case SET_ALL_GAMES: 
-            return {
-                ...state,
-                allGames: action.payload
-            }
-        case SET_GAME_DATA: 
-            return setGameData(action, state)  
-        case UPDATE_GAME_ARRAY:
-            return updateGameArray(action, state)
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case SET_ALL_GAMES:
+      return {
+        ...state,
+        allGames: action.payload,
+      };
+    case UPDATE_GAME_ARRAY:
+      return updateGameArray(action, state);
+    case DELETE_GAME:
+      return updateGameArrayAfterDeletingTheGame(action, state);
+    default:
+      return state;
+  }
+};
 
 export default gamesReducer;
