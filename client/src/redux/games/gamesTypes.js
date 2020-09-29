d93aa3d99b303fa985a2c4c5614be85c05b799e6
Dzianis Maslouski <Dzianis.Maslouski@itechart-group.com>
2020-09-29 20:45:59 +0300

    Deleted unnecessary type and added new type

--- client/src/redux/games/gamesTypes.js
+++ client/src/redux/games/gamesTypes.js
@@ -1,3 +1,3 @@
 export const SET_ALL_GAMES = 'SET_ALL_GAMES';
-export const SET_GAME_DATA = 'SET_GAME_DATA';
 export const UPDATE_GAME_ARRAY = 'UPDATE_GAME_ARRAY';
+export const DELETE_GAME = 'DELETE_GAME';
