143c7604c8ccc73ecc7c228a946f3f7da7412b5b
Dzianis Maslouski <Dzianis.Maslouski@itechart-group.com>
2020-10-29 23:20:28 +0300

    dispay games regarding filtration

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,40 +7,77 @@ import "../styles/content.scss";
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const { allGames } = useSelector((state) => state.games);
   useEffect(() => {
-    dispatch(getAllGames())
+    dispatch(getAllGames());
   }, []);
 
-  return (
-    <main className="container">
-      <div className="content">
-        {allGames &&
-          allGames.map((game) => (
-            <button className="content__card-btn" key={game._id}>
-              <Link
-                to={`/selectedgame/${game._id}`}
-                className="link"
-              >
-                <div className="link__card card">
-                  <img
-                    src={image}
-                    className="card__picture"
-                    alt={game.gameName}
-                  />
-                  <div className="card_description">
-                    <h2>{game.gameName}</h2>
-                    <h3>{game.genre}</h3>
-                    <h3>{game.author}</h3>
-                    <p>{game.price} $</p>
+  const { allGames, filteredGames } = useSelector((state) => state.games);
+
+  if (filteredGames.length && filteredGames[0] === "No matches found.") {
+    return (
+      <main className="container">
+        <div className="content">
+          <h2>{filteredGames[0]}</h2>
+        </div>
+      </main>
+    );
+  }
+
+  if (filteredGames.length && filteredGames[0] !== "No matches found.") {
+    return (
+      <main className="container">
+        <div className="content">
+          {filteredGames &&
+            filteredGames.map((game) => (
+              <button className="content__card-btn" key={game._id}>
+                <Link to={`/selectedgame/${game._id}`} className="link">
+                  <div className="link__card card">
+                    <img
+                      src={image}
+                      className="card__picture"
+                      alt={game.gameName}
+                    />
+                    <div className="card_description">
+                      <h2>{game.gameName}</h2>
+                      <h3>{game.genre}</h3>
+                      <h3>{game.author}</h3>
+                      <p>{game.price} $</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </button>
-          ))}
-      </div>
-    </main>
-  );
+                </Link>
+              </button>
+            ))}
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main className="container">
+        <div className="content">
+          {allGames &&
+            allGames.map((game) => (
+              <button className="content__card-btn" key={game._id}>
+                <Link to={`/selectedgame/${game._id}`} className="link">
+                  <div className="link__card card">
+                    <img
+                      src={image}
+                      className="card__picture"
+                      alt={game.gameName}
+                    />
+                    <div className="card_description">
+                      <h2>{game.gameName}</h2>
+                      <h3>{game.genre}</h3>
+                      <h3>{game.author}</h3>
+                      <p>{game.price} $</p>
+                    </div>
+                  </div>
+                </Link>
+              </button>
+            ))}
+        </div>
+      </main>
+    );
+  }
 };
 
 export default HomePage;
