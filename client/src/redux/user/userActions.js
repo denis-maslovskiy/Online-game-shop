30154896d1aa81f0bd89d094b70c801c1c992ddb
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-01-05 22:02:13 +0300

    personal-discount-and-admin-realization-|-prettier-code-/-removed-unnecessary-code-and-added-new-function

--- client/src/redux/user/userActions.js
+++ client/src/redux/user/userActions.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-import {
-  successMessage,
-  errorMessage,
-  infoMessage,
-} from "../notification/notificationActions";
+import { successMessage, errorMessage, infoMessage } from "../notification/notificationActions";
 
 export const getUserData = (userId) => {
   return async () => {
@@ -31,10 +27,7 @@ export const addGameInTheBasket = (userId, user) => {
 export const purchaseGame = (userId, user) => {
   return async (dispatch) => {
     try {
-      const response = await axios.put(
-        `/api/user/purchasegame/${userId}`,
-        user
-      );
+      const response = await axios.put(`/api/user/purchase-game/${userId}`, user);
       dispatch(successMessage(response.data.message));
     } catch (e) {
       console.log(e.response.data.message);
@@ -42,13 +35,10 @@ export const purchaseGame = (userId, user) => {
   };
 };
 
-export const updateTheBasket = (userId, user) => {
+export const removeGameFromBasket = (userId, user) => {
   return async (dispatch) => {
     try {
-      const response = await axios.put(
-        `/api/user/updatethebasket/${userId}`,
-        user
-      );
+      const response = await axios.delete(`/api/user/remove-game-from-basket/${userId}`, { data: { ...user } });
       dispatch(infoMessage(response.data.message));
     } catch (e) {
       console.log(e.response.data.message);
