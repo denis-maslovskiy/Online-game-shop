e24416b49eb75b2d56cab19a6522d031e91e678a
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-01-05 21:58:45 +0300

    personal-discount-and-admin-realization-|-replaced-put-method-with-delete-and-changed-path-name

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -19,10 +19,7 @@ router.put("/:id", async (req, res) => {
   try {
     const checkIsGameAlreadyExist = await User.findOne({ _id: req.params.id });
     checkIsGameAlreadyExist.gamesInTheBasket.forEach((obj) => {
-      if (
-        obj.gameName ===
-        req.body.gamesInTheBasket[req.body.gamesInTheBasket.length - 1].gameName
-      ) {
+      if (obj.gameName === req.body.gamesInTheBasket[req.body.gamesInTheBasket.length - 1].gameName) {
         return res.status(400).json({
           message: "Such game already exist in the basket.",
         });
@@ -35,33 +32,19 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-// поменять на delete
-router.put("/updatethebasket/:id", async (req, res) => {
+router.delete("/remove-game-from-basket/:id", async (req, res) => {
   try {
     await User.findByIdAndUpdate({ _id: req.params.id }, req.body);
-    const user = User.findOne({ _id: req.params.id })
-    res
-      .status(200)
-      .json({ message: "The game has been removed from the basket" });
+    res.status(200).json({ message: "The game has been removed from the basket" });
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }
 });
 
-router.put("/purchasegame/:id", async (req, res) => {
+router.put("/purchase-game/:id", async (req, res) => {
   try {
     await User.findByIdAndUpdate({ _id: req.params.id }, req.body);
-    res
-      .status(200)
-      .json({ message: "You have successfully purchased the game (s)." });
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-});
-
-router.put("/changeuserdata/:id", async (req, res) => {
-  try {
-    await User.findByIdAndUpdate({ _id: req.params.id }, req.body);
+    res.status(200).json({ message: "You have successfully purchased the game (s)." });
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }
