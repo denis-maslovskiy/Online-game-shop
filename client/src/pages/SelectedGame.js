29e39d4ed0a8f78dbbf51797f755089de693e8db
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-06 01:19:36 +0300

    achievements-and-discount-system-|-switched-react-redux-hooks-and-other-small-changes

--- client/src/pages/SelectedGame.js
+++ client/src/pages/SelectedGame.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { updateGameData } from "../redux/games/gamesActions";
+import { clearErrorMessage, clearSuccessMessage } from "../redux/notification/notificationActions";
 import { addGameInTheBasket, getUserData } from "../redux/user/userActions";
-import { getGameInfo } from '../helpers/gameHelpers';
+import Notification from "../components/Notification";
+import { getGameInfo } from "../helpers/gameHelpers";
 import img1 from "../img/1.jpg";
 import img2 from "../img/2.jpg";
 import img3 from "../img/3.jpg";
@@ -13,16 +15,8 @@ import img6 from "../img/6.jpg";
 import "../styles/selected-game.scss";
 import "../styles/carousel.scss";
 
-import Notification from "../components/Notification";
-import {
-  clearErrorMessage,
-  clearSuccessMessage,
-} from "../redux/notification/notificationActions";
-
-const SelectedGame = (props) => {
-  const [isReadyToDisplayGameInfo, setIsReadyToDisplayGameInfo] = useState(
-    false
-  );
+const SelectedGame = () => {
+  const [isReadyToDisplayGameInfo, setIsReadyToDisplayGameInfo] = useState(false);
   const [isPhysical, setIsPhysical] = useState(false);
   const [isDigital, setIsDigital] = useState(false);
   const [textFields, setTextFields] = useState([
@@ -39,18 +33,17 @@ const SelectedGame = (props) => {
     },
   ]);
   const [gameData, setGameData] = useState(null);
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+  const { successMsg, errorMsg } = useSelector((state) => state.notification);
+  const userId = JSON.parse(localStorage.getItem("userData")).userId;
+
+  useEffect(() => {
+    dispatch(getUserData(userId));
+  }, []);
 
   const history = useHistory();
-
-  const {
-    getUserData,
-    updateGameData,
-    addGameInTheBasket,
-    errorMsg,
-    successMsg,
-    clearErrorMessage,
-    clearSuccessMessage,
-  } = props;
 
   const gameId = window.location.href.split("/")[4];
 
@@ -69,38 +62,37 @@ const SelectedGame = (props) => {
 
       // Increase game rating
       game.rating = game.rating + 1;
-      await updateGameData(game._id, game);
+      dispatch(updateGameData(game._id, game));
     }
     func();
   }, [textFields, getGameInfo, gameId]);
 
   const addToBasketButtonHandler = (gameType) => {
-    clearSuccessMessage();
-    clearErrorMessage();
+    dispatch(clearSuccessMessage());
+    dispatch(clearErrorMessage());
     if (!!JSON.parse(localStorage.getItem("userData"))) {
-      let userId = JSON.parse(localStorage.getItem("userData")).userId;
       const briefInformationAboutTheGame = {
         gameName: gameData.gameName,
         price: gameData.price,
         dateAddedToBasket: new Date(),
         gameId: gameData._id,
         gameType: gameType,
-        discount: gameData.discount
+        discount: gameData.discount,
       };
-      (async function () {
-        const user = await getUserData(userId);
+      if (Object.keys(user).length !== 0) {
         user.gamesInTheBasket.push(briefInformationAboutTheGame);
-        const response = await addGameInTheBasket(userId, user);
-        if (response && briefInformationAboutTheGame.gameType === 'Physical') {
-          gameData.numberOfPhysicalCopies = gameData.numberOfPhysicalCopies - 1;
-          await updateGameData(gameId, gameData);
-          setGameData(gameData);
-        };
-        
-        // Increase game rating
-        gameData.rating = gameData.rating + 10;
-        await updateGameData(gameData._id, gameData);
-      })();
+        dispatch(addGameInTheBasket(userId, user));
+      }
+
+      if (briefInformationAboutTheGame.gameType === "Physical") {
+        gameData.numberOfPhysicalCopies = gameData.numberOfPhysicalCopies - 1;
+        dispatch(updateGameData(gameId, gameData));
+        setGameData(gameData);
+      }
+
+      // Increase game rating
+      gameData.rating = gameData.rating + 10;
+      dispatch(updateGameData(gameData._id, gameData));
     } else {
       history.push("/authorization");
     }
@@ -114,6 +106,8 @@ const SelectedGame = (props) => {
     { img: img5, id: "r5" },
     { img: img6, id: "r6" },
   ];
+
+  const price = (gameData?.price * (1 - gameData?.discount / 100)).toFixed(2);
 
   return (
     <>
@@ -155,9 +149,7 @@ const SelectedGame = (props) => {
           {isReadyToDisplayGameInfo &&
             textFields.map((item) => (
               <div key={item.title}>
-                <span className="game-info__text-field-title">
-                  {item.title}
-                </span>
+                <span className="game-info__text-field-title">{item.title}</span>
                 <p className="game-info__text-field-value">{item.value}</p>
               </div>
             ))}
@@ -165,12 +157,9 @@ const SelectedGame = (props) => {
         <div className="content-area__buy-game buy-game">
           <div className="buy-game__digital-copy">
             <h2 className="buy-game__title">Digital Copy</h2>
-            {isReadyToDisplayGameInfo && <h2 className="buy-game__price">Price {(gameData?.price * (1-(gameData?.discount/100))).toFixed(2)}$</h2>}
+            {isReadyToDisplayGameInfo && <h2 className="buy-game__price">Price {price}$</h2>}
             {isDigital && (
-              <button
-                className="buy-game__button"
-                onClick={() => addToBasketButtonHandler("Digital")}
-              >
+              <button className="buy-game__button" onClick={() => addToBasketButtonHandler("Digital")}>
                 Add to basket
               </button>
             )}
@@ -182,12 +171,9 @@ const SelectedGame = (props) => {
           </div>
           <div className="buy-game__physical-copy">
             <h2 className="buy-game__title">Physical Copy</h2>
-            {isReadyToDisplayGameInfo && <h2 className="buy-game__price">Price {(gameData?.price * (1-(gameData?.discount/100))).toFixed(2)}$</h2>}
+            {isReadyToDisplayGameInfo && <h2 className="buy-game__price">Price {price}$</h2>}
             {isPhysical && (
-              <button
-                className="buy-game__button"
-                onClick={() => addToBasketButtonHandler("Physical")}
-              >
+              <button className="buy-game__button" onClick={() => addToBasketButtonHandler("Physical")}>
                 Add to basket
               </button>
             )}
@@ -203,22 +189,4 @@ const SelectedGame = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    errorMsg: state.notification.errorMsg,
-    successMsg: state.notification.successMsg,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getUserData: (userId) => dispatch(getUserData(userId)),
-    updateGameData: (userId, game) => dispatch(updateGameData(userId, game)),
-    addGameInTheBasket: (userId, gameData) =>
-      dispatch(addGameInTheBasket(userId, gameData)),
-    clearErrorMessage: () => dispatch(clearErrorMessage()),
-    clearSuccessMessage: () => dispatch(clearSuccessMessage()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(SelectedGame);
+export default SelectedGame;
