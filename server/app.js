fdc6bd9be570965de648554e56f16c9f398d23fb
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-06 01:15:33 +0300

    achievements-and-discount-system-|-added-achievement

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/games", require("./routes/games.routes"));
 app.use("/api/user", require("./routes/user.routes"));
 app.use("/api/admin", require("./routes/admin.routes"));
+app.use("/api/achievement", require("./routes/achievement.routes"));
 
 const PORT = config.get("port") || 5000;
 
