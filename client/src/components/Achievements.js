6e3698e1d472c2b9ebf3f5545b4cab17647e2649
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-06 01:25:53 +0300

    achievements-and-discount-system-|-removed-unnecessary-code-|-started-using-redux-and-achievements-from-DB

--- client/src/components/Achievements.js
+++ client/src/components/Achievements.js
@@ -1,125 +1,88 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CachedIcon from "@material-ui/icons/Cached";
+import { getAllAchievements } from "../redux/achievement/achievementActions";
 import { updateUserData } from "../helpers/userHelpers";
 
-const minimumValueOfMoneySpent = 50,
-  minimumValueOfPurchasedGames = 1,
-  minimumValueOfPurchasedDigitalGames = 5,
-  minimumValueOfPurchasedPhysicalGames = 5;
-
-export const AchievementsList = {
-  achievingMoneySpent: {
-    spend50Dollars: {
-      value: minimumValueOfMoneySpent,
-      achievementText: "Buy games for 50 $",
-      estimatedDiscountForTheClient: 2,
-    },
-    spend100Dollars: {
-      value: 100,
-      achievementText: "Buy games for 100 $",
-      estimatedDiscountForTheClient: 4,
-    },
-    spend1000Dollars: {
-      value: 1000,
-      achievementText: "Buy games for 1000 $",
-      estimatedDiscountForTheClient: 6,
-    },
-  },
-  achievingPurchasedGames: {
-    purchased1Game: {
-      value: minimumValueOfPurchasedGames,
-      achievementText: "Buy one game",
-      estimatedDiscountForTheClient: 0,
-    },
-    purchased5Game: {
-      value: 5,
-      achievementText: "Buy 5 games",
-      estimatedDiscountForTheClient: 2,
-    },
-    purchased10Game: {
-      value: 10,
-      achievementText: "Buy 10 games",
-      estimatedDiscountForTheClient: 6,
-    },
-  },
-  loverOfDigitalGames: {
-    purchased5DigitalGame: {
-      value: minimumValueOfPurchasedDigitalGames,
-      achievementText: "Buy 5 digital games",
-      estimatedDiscountForTheClient: 2,
-    },
-    purchased10DigitalGame: {
-      value: 10,
-      achievementText: "Buy 10 digital games",
-      estimatedDiscountForTheClient: 4,
-    },
-  },
-  loverOfPhysicalGames: {
-    purchased5PhysicalGame: {
-      value: minimumValueOfPurchasedPhysicalGames,
-      achievementText: "Buy 5 physical games",
-      estimatedDiscountForTheClient: 2,
-    },
-    purchased10PhysicalGame: {
-      value: 10,
-      achievementText: "Buy 10 physical games",
-      estimatedDiscountForTheClient: 4,
-    },
-  },
-};
-
-const Achievements = ({ user, UpdatingUserDataTriggered }) => {
+const Achievements = ({ user, updatingUserDataTriggered }) => {
   const [buttonToggle, setButtonToggle] = useState(false);
-  const [userData, setUserData] = useState(user);
-  let gamesBoughtFor = 0,
-    numberOfGamePurchased = 0,
-    numberOfPurchasedPhysicalCopiesOfTheGame = 0,
-    numberOfPurchasedDigitalCopiesOfTheGame = 0,
-    personalDiscount = 0;
+  const dispatch = useDispatch();
+
+  const { allAchievements } = useSelector((state) => state.achievement);
+
+  useEffect(() => {
+    dispatch(getAllAchievements());
+  }, []);
 
   const reloadAchievementsClickHandler = () => {
+    let gamesBoughtFor = 0,
+      numberOfGamePurchased = 0,
+      numberOfPurchasedPhysicalGames = 0,
+      numberOfPurchasedDigitalGames = 0,
+      personalDiscount = 0;
+
+    const minimumValueOfMoneySpent = 50,
+      minimumValueOfPurchasedGames = 1,
+      minimumValueOfPurchasedDigitalGames = 5,
+      minimumValueOfPurchasedPhysicalGames = 5;
+
+    const ACHIEVING_MONEY_SPENT = "achievingMoneySpent",
+      ACHIEVING_PURCHASED_GAMES = "achievingPurchasedGames",
+      LOVER_OF_DIGITAL_GAMES = "loverOfDigitalGames",
+      LOVER_OF_PHYSICAL_GAMES = "loverOfPhysicalGames";
+
     if (user?.purchasedGames.length) {
       numberOfGamePurchased += user.purchasedGames.length;
 
       user.purchasedGames.forEach((game) => {
         gamesBoughtFor += game.price;
-        game.gameType === "Physical"
-          ? numberOfPurchasedPhysicalCopiesOfTheGame++
-          : numberOfPurchasedDigitalCopiesOfTheGame++;
+        game.gameType === "Physical" ? numberOfPurchasedPhysicalGames++ : numberOfPurchasedDigitalGames++;
       });
     }
 
-    const achievements = [];
+    const achievementsResult = [];
+    const achievementsObj = {};
 
-    const countingAchievements = (topic, value, minimumValue) => {
-      for (let key in AchievementsList[topic]) {
-        if (AchievementsList[topic][key].value <= value) {
-          if (AchievementsList[topic][key].value === minimumValue) {
-            achievements.push(AchievementsList[topic][key]);
+    allAchievements.forEach((achieve) => {
+      achievementsObj[achieve.achievementTopic] = {};
+    });
+    allAchievements.forEach((achieve) => {
+      achievementsObj[achieve.achievementTopic][achieve.achievementName] = {
+        value: achieve.achievementValue,
+        achievementText: achieve.achievementText,
+        estimatedDiscountForTheClient: achieve.estimatedDiscountForTheClient,
+      };
+    });
+
+    const handleCountAchievements = (topic, value, minimumValue) => {
+      for (let key in achievementsObj[topic]) {
+        if (achievementsObj[topic][key].value <= value) {
+          if (achievementsObj[topic][key].value === minimumValue) {
+            achievementsResult.push(achievementsObj[topic][key]);
           } else {
-            achievements.pop();
-            achievements.push(AchievementsList[topic][key]);
+            achievementsResult.pop();
+            achievementsResult.push(achievementsObj[topic][key]);
           }
         } else break;
       }
     };
 
-    for (let topic in AchievementsList) {
+    for (let topic in achievementsObj) {
       switch (topic) {
-        case "achievingMoneySpent": {
-          countingAchievements(topic, gamesBoughtFor, minimumValueOfMoneySpent);
+        case ACHIEVING_MONEY_SPENT: {
+          handleCountAchievements(topic, gamesBoughtFor, minimumValueOfMoneySpent);
           break;
         }
-        case "achievingPurchasedGames": {
-          countingAchievements(topic, numberOfGamePurchased, minimumValueOfPurchasedGames);
+        case ACHIEVING_PURCHASED_GAMES: {
+          handleCountAchievements(topic, numberOfGamePurchased, minimumValueOfPurchasedGames);
           break;
         }
-        case "loverOfDigitalGames": {
-          countingAchievements(topic, numberOfPurchasedDigitalCopiesOfTheGame, minimumValueOfPurchasedDigitalGames);
+        case LOVER_OF_DIGITAL_GAMES: {
+          handleCountAchievements(topic, numberOfPurchasedDigitalGames, minimumValueOfPurchasedDigitalGames);
           break;
         }
-        case "loverOfPhysicalGames": {
-          countingAchievements(topic, numberOfPurchasedPhysicalCopiesOfTheGame, minimumValueOfPurchasedPhysicalGames);
+        case LOVER_OF_PHYSICAL_GAMES: {
+          handleCountAchievements(topic, numberOfPurchasedPhysicalGames, minimumValueOfPurchasedPhysicalGames);
           break;
         }
         default:
@@ -127,26 +90,24 @@ const Achievements = ({ user, UpdatingUserDataTriggered }) => {
       }
     }
 
-    user.achievements = achievements;
-    achievements.forEach((achieve) => {
+    user.achievements = achievementsResult;
+    achievementsResult.forEach((achieve) => {
       personalDiscount += achieve.estimatedDiscountForTheClient;
     });
     user.personalDiscount = personalDiscount;
 
-    setUserData(user);
-
     gamesBoughtFor = 0;
     numberOfGamePurchased = 0;
-    numberOfPurchasedPhysicalCopiesOfTheGame = 0;
-    numberOfPurchasedDigitalCopiesOfTheGame = 0;
+    numberOfPurchasedPhysicalGames = 0;
+    numberOfPurchasedDigitalGames = 0;
     personalDiscount = 0;
 
     setButtonToggle((prevState) => !prevState);
   };
 
   useEffect(() => {
-    updateUserData(userData._id, userData);
-    UpdatingUserDataTriggered();
+    updateUserData(user._id, user);
+    updatingUserDataTriggered();
   }, [buttonToggle]);
 
   return (
