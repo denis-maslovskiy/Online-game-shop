86dabe8c90e157ee4d6ce755c036bc302884a691
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-06 01:16:57 +0300

    achievements-and-discount-system-|-updated-reducer-with-new-functionality

--- client/src/redux/user/userReducer.js
+++ client/src/redux/user/userReducer.js
@@ -1,19 +1,25 @@
-import {  SET_ALL_USERS } from './userTypes';
+import { SET_ALL_USERS, SET_USER_DATA } from "./userTypes";
 
 const initialState = {
-    allUsers: []
-}
+  allUsers: [],
+  user: {},
+};
 
 const userReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case SET_ALL_USERS:
-            return {
-                ...state,
-                allUsers: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case SET_ALL_USERS:
+      return {
+        ...state,
+        allUsers: action.payload,
+      };
+    case SET_USER_DATA:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
