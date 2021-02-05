541a27be49fc6243346aad2bf027d12d57280358
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-06 01:17:54 +0300

    achievements-and-discount-system-|-added-achievement-reducer

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import notificationReducer from "./notification/notificationReducer";
 import authenticationReducer from "./authentication/authenticationReducer";
 import gamesReducer from "./games/gamesReducer";
-import userReducer from './user/userReducer';
+import userReducer from "./user/userReducer";
+import achievementReducer from "./achievement/achievementReducer";
 
 const rootReducer = combineReducers({
   notification: notificationReducer,
   authentication: authenticationReducer,
   games: gamesReducer,
-  user: userReducer
+  user: userReducer,
+  achievement: achievementReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
