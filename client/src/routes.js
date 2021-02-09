169fabf16dcf1f350ecc15a01d24ef60457bff79
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-09 17:08:41 +0300

    game-author-|-added-gameAuthor-page-to-application-routes-system

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,6 +7,7 @@ import Basket from "./pages/Basket";
 import PersonalAccount from "./pages/PersonalAccount";
 import Authorization from "./pages/Authorization";
 import Admin from "./pages/Admin/Admin";
+import SelectedGameAuthor from "./pages/SelectedGameAuthor";
 import { PrivateRoute, AdminPrivateRoute } from "./PrivateRoutes";
 import { useAuth } from "./hooks/authHook";
 
@@ -18,6 +19,7 @@ export const useRoutes = () => {
       <Route path="/registration" component={Registration} exact />
       <Route path="/authorization" component={Authorization} exact />
       <Route path="/selected-game/:id" component={SelectedGame} exact />
+      <Route path="/selected-game-author/:id" component={SelectedGameAuthor} exact />
       <PrivateRoute path="/basket" isAuthenticated={isAuthenticated} component={Basket} exact />
       <PrivateRoute path="/account" isAuthenticated={isAuthenticated} component={PersonalAccount} exact />
       <AdminPrivateRoute
