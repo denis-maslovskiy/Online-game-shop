033b540e1e94105058fe7cd1e71b5d483c9c9891
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-09 17:09:01 +0300

    game-author-|-added-gameAuthor-reducer

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import notificationReducer from "./notification/notificationReducer";
 import authenticationReducer from "./authentication/authenticationReducer";
 import gamesReducer from "./games/gamesReducer";
-import userReducer from './user/userReducer';
+import userReducer from "./user/userReducer";
+import gameAuthorReducer from "./gameAuthor/gameAuthorReducer";
 
 const rootReducer = combineReducers({
   notification: notificationReducer,
   authentication: authenticationReducer,
   games: gamesReducer,
-  user: userReducer
+  user: userReducer,
+  gameAuthor: gameAuthorReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
