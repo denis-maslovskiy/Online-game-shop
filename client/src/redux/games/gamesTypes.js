66a0b6398e7fd7fe9556b749ae3b7cbf24a62123
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-17 23:44:03 +0300

    image-upload-with-cloudinary-|-addded-new-types

--- client/src/redux/games/gamesTypes.js
+++ client/src/redux/games/gamesTypes.js
@@ -3,3 +3,5 @@ export const UPDATE_GAME_ARRAY = 'UPDATE_GAME_ARRAY';
 export const DELETE_GAME = 'DELETE_GAME';
 export const GAME_FILTER = 'GAME_FILTER';
 export const GAME_SORT = 'GAME_SORT'
+export const SET_GAME_DATA = 'SET_GAME_DATA';
+export const CLEAR_GAME_DATA = 'CLEAR_GAME_DATA';
