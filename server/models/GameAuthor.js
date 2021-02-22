9320062a1dd30d89d58ee93349a17437d95b0a82
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-22 21:06:03 +0300

    image-upload-with-cloudinary-|-updated-gameAuthor-model

--- server/models/GameAuthor.js
+++ server/models/GameAuthor.js
@@ -4,7 +4,8 @@ const schema = new Schema({
     authorName: { type: String, required: true, unique: true },
     authorDescription: { type: String, default: "No information" },
     authorsGames: { type: Array, default: [] },
-    yearOfFoundationOfTheCompany: { type: Date, default: new Date() }
+    yearOfFoundationOfTheCompany: { type: Date, default: new Date() },
+    authorLogo: { type: String, default: "" }
 });
 
 module.exports = model("GameAuthor", schema);
