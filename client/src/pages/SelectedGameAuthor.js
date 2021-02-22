2e1a0f7e5262ba4aaefff6e94a69b052ab4e6b73
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-22 21:11:33 +0300

    image-upload-with-cloudinary-|-made-updated-related-to-images-and-cloudinary

--- client/src/pages/SelectedGameAuthor.js
+++ client/src/pages/SelectedGameAuthor.js
@@ -1,51 +1,56 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getSelectedGameAuthor } from '../redux/gameAuthor/gameAuthorActions';
-import { getAllGames } from '../redux/games/gamesActions';
+import { Image } from "cloudinary-react";
+import { getSelectedGameAuthor } from "../redux/gameAuthor/gameAuthorActions";
+import { getAllGames } from "../redux/games/gamesActions";
 
 const SelectedGameAuthor = () => {
-    const dispatch = useDispatch();
-    const hrefSplittedArray = window.location.href.split('/');
-    const gameAuthorId = hrefSplittedArray[hrefSplittedArray.length - 1];
+  const dispatch = useDispatch();
+  const hrefSplittedArray = window.location.href.split("/");
+  const gameAuthorId = hrefSplittedArray[hrefSplittedArray.length - 1];
 
-    useEffect(() => {
-        dispatch(getSelectedGameAuthor(gameAuthorId));
-        dispatch(getAllGames());
-    }, []);
+  useEffect(() => {
+    dispatch(getSelectedGameAuthor(gameAuthorId));
+    dispatch(getAllGames());
+  }, []);
 
-    const { selectedGameAuthor } = useSelector(state => state.gameAuthor);
-    const { allGames } = useSelector(state => state.games);
+  const { selectedGameAuthor } = useSelector((state) => state.gameAuthor);
+  const { allGames } = useSelector((state) => state.games);
 
-    const getGameId = (gameName, gameId) => {
-        if(gameId) {
-            return gameId;
-        } 
-        
-        let setGameId = "";
-        allGames.forEach(game => {
-            if(game.gameName === gameName) {
-                setGameId = game._id;
-            }
-        });
-        return setGameId;
+  const getGameId = (gameName, gameId) => {
+    if (gameId) {
+      return gameId;
     }
 
-    return (
-        <>
-            {selectedGameAuthor?.authorsGames?.length && selectedGameAuthor?.authorsGames?.map(game => {
-                return (
-                    <div key={game.gameName}>
-                        <p>{game.gameName}</p>
-                        <p>{game.gameDescription}</p>
-                        <p>{game.releaseDate}</p>
-                        <p>{game.genre}</p>
-                        <Link to={`/selected-game/${getGameId(game.gameName, game._id)}`}>Game Link</Link>
-                    </div>
-                )
-            })}
-        </>
-    )
-}
+    let setGameId = "";
+    allGames.forEach((game) => {
+      if (game.gameName === gameName) {
+        setGameId = game._id;
+      }
+    });
+    return setGameId;
+  };
+
+  return (
+    <>
+      {selectedGameAuthor?.authorLogo && (
+        <Image cloudName="dgefehkt9" publicId={selectedGameAuthor?.authorLogo} width="300" />
+      )}
+      {selectedGameAuthor?.authorsGames?.length &&
+        selectedGameAuthor?.authorsGames?.map((game) => {
+          return (
+            <div key={game.gameName}>
+              <p>{game.gameName}</p>
+              <p>{game.gameDescription}</p>
+              <p>{game.releaseDate}</p>
+              <p>{game.genre}</p>
+              <Link to={`/selected-game/${getGameId(game.gameName, game._id)}`}>Game Link</Link>
+            </div>
+          );
+        })}
+    </>
+  );
+};
 
 export default SelectedGameAuthor;
