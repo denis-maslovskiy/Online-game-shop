28e9332a216ed01aea9dce272c653a5c6051ff45
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-24 21:35:43 +0300

    image-upload-with-cloudinary-|-renamed-Context.Provider-/-added-new-dependencies-into-provider-\-prettier-code

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import { useRoutes } from "./routes";
 import { Navbar } from "./components/Navbar";
 import { useAuth } from "./hooks/authHook";
-import { AuthContext } from "./context/AuthContext";
+import { DependenciesContext } from "./context/DependenciesContext";
 import "./styles/body.scss";
 
 function App() {
-  const { token, login, logout, userId } = useAuth();
+  const { token, login, logout, userId, isAdmin } = useAuth();
   const isAuthenticated = !!token;
   const routes = useRoutes();
+  const cloudName = "dgefehkt9";
 
   return (
-    <AuthContext.Provider
-      value={{ token, login, logout, userId, isAuthenticated }}
-    >
+    <DependenciesContext.Provider value={{ token, login, logout, userId, isAuthenticated, isAdmin, cloudName }}>
       <BrowserRouter>
         <Navbar />
         <div>{routes}</div>
       </BrowserRouter>
-    </AuthContext.Provider>
+    </DependenciesContext.Provider>
   );
 }
 
