187db6916ba0b26e3ceced3b0340bab1ce45f71f
ICX\Dzianis.Maslouski <Dzianis.Maslouski@itechart-group.com>
2021-02-25 17:33:34 +0300

    change-the-system-of-accrual-of-achievements-|-added-<user>-as-a-part-of-response

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -44,7 +44,7 @@ router.delete("/remove-game-from-basket/:id", async (req, res) => {
 router.put("/purchase-game/:id", async (req, res) => {
   try {
     await User.findByIdAndUpdate({ _id: req.params.id }, req.body);
-    res.status(200).json({ message: "You have successfully purchased the game (s)." });
+    res.status(200).json({ message: "You have successfully purchased the game (s).", user: req.body });
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }
